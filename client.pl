f03849ef771755025cb3d38d3e705d425b00928e
Kristian Nielsen <knielsen@knielsen-hq.org>
2014-12-07 16:25:13 +0100

    Implement code to insert the data received into the Postgresql database.

--- client.pl
+++ client.pl
@@ -3,7 +3,67 @@
 use strict;
 use warnings;
 
+use DBI;
+
 my @devices_active;
+
+
+my $dbh = DBI->connect("DBI:Pg:dbname=powermeter", "powermeter", undef,
+                       {RaiseError=>1, AutoCommit=>1});
+
+sub unquote {
+  my ($x) = @_;
+  $x =~ s/\\([0-9a-fA-F][0-9a-fA-F])/chr(hex($1))/ge;
+  return $x;
+}
+
+
+sub device_active {
+  my ($dev, $poll_interval, $description, $unit) = @_;
+
+  my $res = $dbh->selectall_arrayref(<<SQL, undef, $dev);
+SELECT active, description, unit, poll_interval
+  FROM device_status
+ WHERE id = ?
+SQL
+  # Insert a row if new status differs from existing.
+  if (!scalar(@$res) ||
+      !$res->[0][0] ||
+      $res->[0][1] ne $description ||
+      $res->[0][2] ne $unit ||
+      $res->[0][3] != $poll_interval) {
+    $dbh->do(<<SQL, undef, $dev, $description, $unit, $poll_interval);
+INSERT INTO device_history VALUES (?, NOW(), TRUE, ?, ?, ?)
+SQL
+  }
+}
+
+
+sub device_inactive {
+  my ($dev) = @_;
+
+  my $res = $dbh->selectall_arrayref(<<SQL, undef, $dev);
+SELECT active, description, unit, poll_interval
+  FROM device_status
+ WHERE id = ?
+SQL
+  # Insert an inactive row only if the device is currently listed active.
+  if (scalar(@$res) && $res->[0][0]) {
+    $dbh->do(<<SQL, undef, $dev);
+INSERT INTO device_history VALUES (?, NOW(), FALSE, NULL, NULL, NULL)
+SQL
+  }
+}
+
+
+sub
+device_value {
+  my ($dev, $val) = @_;
+  $dbh->do(<<SQL, undef, $dev, $val);
+INSERT INTO device_log VALUES (?, NOW(), ?)
+SQL
+}
+
 
 open M, '+<', '/dev/ttyACM0'
     or die "Failed to open master device: $!\n";
@@ -17,15 +77,18 @@ while (<M>) {
     print "Device $dev no longer active.\n"
         if $devices_active[$dev];
     undef $devices_active[$dev];
+    device_inactive($dev);
   } elsif (/^ACTIVE ([0-9]+)\|([0-9]+)\|([^|]*)\|([^|]*)$/) {
-    my ($dev, $interval, $desc, $unit) = ($1, $2, $3, $4);
+    my ($dev, $interval, $desc, $unit) = ($1, $2, unquote($3), unquote($4));
     chomp($unit);
     print "Device $dev active: $interval '$desc' '$unit'.\n"
         if !$devices_active[$dev];
     $devices_active[$dev] = 1;
+    device_active($dev, $interval, $desc, $unit);
   } elsif (/^POLL ([0-9]+) (.*)$/) {
     my ($dev, $val) = ($1, $2);
     print "Device $dev: value $val\n";
+    device_value($dev, $val);
   }
   else {
     print "Master said: $_";
